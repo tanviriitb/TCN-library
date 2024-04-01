c21795ae2b26d075a7e6a46ec95750970af90bdc
Tanvir <t.a.harris@student.tudelft.nl>
2024-04-01 09:03:21 +0200

    Pool fix

--- src/tcn_lib/blocks/GlobalAvgPool.py
+++ src/tcn_lib/blocks/GlobalAvgPool.py
@@ -1,11 +1,20 @@
 import torch.nn as nn
 
+class GlobalAvgPool1d(nn.Sequential):
+
+    def __init__(self, kernel_size: int = 201):
+
+        layer = nn.AvgPool1d(kernel_size=kernel_size)
+
+        super(GlobalAvgPool1d, self).__init__(layer)
+
+   
 class GlobalAvgPool(nn.Module):
 
     def __init__(self, kernel_size: int = 201):
         super(GlobalAvgPool, self).__init__()
 
-        self.layer = nn.AvgPool1d(kernel_size=kernel_size)
+        self.layer = GlobalAvgPool1d(kernel_size)
 
     def forward(self, x):
         return self.layer(x)
